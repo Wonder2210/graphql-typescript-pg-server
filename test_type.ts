08fe3a92e76ebfdfa788f862d26ae3bb7fce8bb6
Wonder Gonzalez <gonzalezwonderly@gmail.com>
2022-05-19 01:12:28 -0400

    [chore] remove unused code

--- test_type.ts
+++
@@ -1,5 +0,0 @@
-import UserResolvers from "./src/schema/resolvers/user";
-import { mockResolver } from "./src/__tests__/utils/types_resolvers";
-const user = mockResolver(UserResolvers.Query!.user!);
-
-let userC = user(undefined, { id: 1 }, null);
